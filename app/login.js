fa053c1e1840f99dd3ecb0a2c17b47cc27dfb763
Tee <timmytitee@gmail.com>
2024-02-16 19:36:44 +0700

    login - change UI, add dummy useState

--- app/login.js
+++ app/login.js
@@ -1,32 +1,71 @@
-import { View, Text, TextInput, Button} from "react-native";
-import { Link } from "expo-router";
-
+import { useState } from "react";
+import {
+  NativeBaseProvider,
+  FormControl,
+  Box,
+  Center,
+  Input,
+  Heading,
+  Text,
+  VStack,
+  HStack,
+  Button,
+  Link,
+} from "native-base";
 
 export default function Login() {
-    return(
-    <View>
-        <Text>Login Page</Text>
-        <TextInput 
-        placeholder="Username"
-        style={{backgroundColor: '#FF0000',
-                padding: 20,
-                margin: 20,}}
-        />
-        <TextInput
-        placeholder="Password"
-        style={{backgroundColor: '#FF0000',
-                padding: 20,
-                margin: 20,}}
-        />
+  const [phoneNum, setPhoneNum] = useState("");
 
-        <Link href='/'>
-            <Button title="Login" />
-        </Link>
+  const onSetPhoneNum = (text) => setPhoneNum(text);
 
-        <Link href='/'>
-            <Button title="Sign Up" />
-        </Link>
+  return (
+    <NativeBaseProvider>
+      <Center flex={1}>
+        <Heading
+          size="3xl"
+          fontWeight="600"
+          color="coolGray.800"
+          _dark={{
+            color: "warmGray.50",
+          }}
+        >
+          WhereNext
+        </Heading>
+        <Box safeArea p="2" py="8" w="90%" maxW="290">
 
-    </View>
-    ); 
+          <VStack space={4} mt="5">
+            <FormControl>
+              <FormControl.Label>Phone Number</FormControl.Label>
+              <Input value={phoneNum} onChangeText={onSetPhoneNum} />
+            </FormControl>
+            <Button mt="10px" colorScheme="indigo">
+              Sign in
+            </Button>
+            <HStack mt="6" justifyContent="center">
+              <Text
+                fontSize="sm"
+                color="coolGray.600"
+                _dark={{
+                  color: "warmGray.200",
+                }}
+              >
+                I'm a new user.{" "}
+              </Text>
+              <Link
+                _text={{
+                  color: "indigo.500",
+                  fontWeight: "medium",
+                  fontSize: "sm",
+                }}
+                href="#"
+              >
+                Sign Up
+              </Link>
+            </HStack>
+            <Text>{phoneNum}</Text>
+          </VStack>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
+  );
 }
